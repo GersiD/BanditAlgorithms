fa466b87a1e131ab0a7ad940ce87df339456cf67
git <gd1064@wildcats.unh.edu>
2023-10-04 17:38:47 -0400

    adding some images

--- BA_ch_11/BA11.py
+++ BA_ch_11/BA11.py
@@ -2,6 +2,7 @@ from UCB import UCB
 from EXP3 import EXP3
 import numpy as np
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
 
 class BernouliBandit(object):
     """Implementation of a Bernouli bandit."""
@@ -27,24 +28,39 @@ class BernouliBandit(object):
             # Update the regret.
             regret += self.means[1] - self.means[arm]
         return regret
+    
+    def run_experiment(self, horizon):
+        print(f"Running experiment for horizon = {horizon}...")
+        ucb = UCB(self.n_arms, horizon)
+        exp3 = EXP3(self.n_arms, horizon)
+        ucb_regret = self.run(ucb, horizon)
+        exp3_regret = self.run(exp3, horizon)
+        return ucb_regret, exp3_regret
 
 def main():
     # Set up the experiment.
     # we have a two armed bernoulli bandit with means 0.5 and 0.5 + delta
     delta = 0.05
     bandit = BernouliBandit(delta)
+    print("Setup complete. Running experiments...")
     # plot the regret of the UCB and EXP3 algorithms
     # as a function of the horizon
     # for horizon in range(10, 10^5, 100):
     ucb_regret = []
     exp3_regret = []
-    horizon_list = []
-    for horizon in range(10, 100000, 100):
-        ucb = UCB(bandit.n_arms, horizon)
-        exp3 = EXP3(bandit.n_arms, horizon)
-        ucb_regret.append(bandit.run(ucb, horizon))
-        exp3_regret.append(bandit.run(exp3, horizon))
-        horizon_list.append(horizon)
+    horizon_list = list(range(10, 100000, 1000))
+    with Pool() as p:
+        results = p.map(bandit.run_experiment, range(10, 100000, 1000))
+        for ucb_r, exp3_r in results:
+            ucb_regret.append(ucb_r)
+            exp3_regret.append(exp3_r)
+    # for horizon in range(10, 100000, 1000):
+    #     print(f"Running experiment for horizon = {horizon}...")
+    #     ucb = UCB(bandit.n_arms, horizon)
+    #     exp3 = EXP3(bandit.n_arms, horizon)
+    #     ucb_regret.append(bandit.run(ucb, horizon))
+    #     exp3_regret.append(bandit.run(exp3, horizon))
+    #     horizon_list.append(horizon)
     plt.plot(horizon_list, ucb_regret, label="UCB")
     plt.plot(horizon_list, exp3_regret, label="EXP3")
     plt.xlabel("Horizon")
@@ -52,7 +68,7 @@ def main():
     plt.legend()
     plt.savefig("BA11_A.png")
     plt.clf()
-    
+    print("Plotting complete. Running experiment part B...") 
     # now fix the horizon = 10^5 and plot the regret as a function of eta
     # for eta in range(0.01, 1, 0.01):
     ucb_regret = []
@@ -77,7 +93,8 @@ def main():
     plt.ylabel("Regret")
     plt.legend()
     plt.savefig("BA11_B.png")
-
+    plt.clf()
+    print("Done.")
 
 
 if __name__ == "__main__":
